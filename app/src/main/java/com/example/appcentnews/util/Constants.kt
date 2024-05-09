560ee9ab6967fdfb2363a2b88ab7d5a2654627b6
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2024-05-09 16:20:59 +0300

    Extracted API constants into a separate class

---
+++ app/src/main/java/com/example/appcentnews/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.appcentnews.util
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "b7f7e6f191fa4b0f9c96b0cced74a8fd"
+    }
+}
