a74045fe2193263a783458436a9e646c5f090a42
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2024-05-12 11:11:18 +0300

    Updated NetworkingModule with missing HTTP providers

--- app/src/main/java/com/example/appcentnews/di/NetworkingModule.kt
+++ app/src/main/java/com/example/appcentnews/di/NetworkingModule.kt
@@ -2,25 +2,57 @@ package com.example.appcentnews.di
 
 import com.example.appcentnews.api.NewsService
 import com.example.appcentnews.util.Constants
+import com.example.appcentnews.util.Constants.Companion.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkingModule {
 
+    @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+    fun provideHttpLoggerInterceptor(): HttpLoggingInterceptor {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return httpLoggingInterceptor
+    }
+
+    @Singleton
+    @Provides
+    fun provideHttpClint(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient{
+        return OkHttpClient.Builder().readTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS).addInterceptor(httpLoggingInterceptor)
             .build()
     }
 
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): GsonConverterFactory{
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit{
+        return Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(gsonConverterFactory)
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Singleton
     @Provides
     fun provideNewsService(retrofit: Retrofit): NewsService {
         return retrofit.create(NewsService::class.java)
---
+++ app/src/main/java/com/example/appcentnews/presantation/news_screen/NewsViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.appcentnews.presantation.news_screen
+
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.appcentnews.model.Article
+import com.example.appcentnews.model.NewsResponse
+import com.example.appcentnews.repository.NewsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NewsViewModel @Inject constructor(
+    private val newsRepository: NewsRepository
+) : ViewModel() {
+
+    val newsResponse: MutableLiveData<NewsResponse> = MutableLiveData()
+    val articles = mutableStateOf<List<Article?>>(listOf())
+    var pageNumber = 1
+
+    suspend fun searchForNews(searchQuery: String) {
+        newsResponse.value = newsRepository.searchForNews(searchQuery, pageNumber).body()
+        articles.value = newsResponse.value?.articles!!
+        //filterRemovedArticles()
+    }
+
+    private fun filterRemovedArticles() {
+        val filteredArticles = articles.value.filter { it?.title != "[Removed]" }
+        articles.value = filteredArticles
+    }
+
+    fun loadMoreNews(searchQuery: String) {
+        pageNumber++
+        viewModelScope.launch {
+            val response = newsRepository.searchForNews(searchQuery, pageNumber)
+            if (response.isSuccessful) {
+                newsResponse.value = response.body()
+                articles.value += newsResponse.value?.articles!!
+                filterRemovedArticles()
+            }
+        }
+    }
+
+
+}
