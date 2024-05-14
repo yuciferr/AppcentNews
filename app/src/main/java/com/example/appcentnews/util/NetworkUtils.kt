2b6eeeddadea4ed6d551efadf97fbe5b78b27abe
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2024-05-14 16:39:41 +0300

    Implemented network connectivity check using NetworkUtils

---
+++ app/src/main/java/com/example/appcentnews/util/NetworkUtils.kt
@@ -0,0 +1,17 @@
+package com.example.appcentnews.util
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+object NetworkUtils {
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+        return networkCapabilities != null &&
+                (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                        networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
+    }
+}
